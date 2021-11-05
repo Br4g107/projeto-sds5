2d9bc3599154ff5ee535345c0d592e384c77256d
Br4g107 <diego_jf@live.com>
2021-11-05 13:29:22 -0300

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -18,11 +18,27 @@ function DataTable() {
                 <td>34</td>
                 <td>25</td>
                 <td>15017.00</td>
+                 <td>22/04/2021</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+                 <td>22/04/2021</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+                 <td>22/04/2021</td>
             </tr>
         </tbody>
     </table>
 </div>
-
   );
 }
 
